834957f93af3ac21fe5100ca8b0d1663a197acd7
denisemenov <hello@denisemenov.com>
2025-01-29 13:18:00 +0000

    Deploying to gh-pages from @ denisemenov/legendbc.ru@571881083a74f74f4b3ce05db63794352140cd71 🚀

--- _nuxt/ChACsTGp.js
+++
@@ -1,1 +0,0 @@
-import{l as s,k as r,f as t,L as n}from"./BxH68ZES.js";const o={};function a(e,c){return r(),t("th",null,[n(e.$slots,"default")])}const _=Object.assign(s(o,[["render",a]]),{__name:"ProseTh"});export{_ as default};
